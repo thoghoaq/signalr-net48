3b27ed8c02461f7fa057b77923947991f71e7bc2
HOANG Trung Thong <thoang5@mantu.com>
2023-07-19 15:55:17 +0700

    init src

---
+++ SignalRWeb/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRWeb
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string user, string message)
+        {
+            // Broadcast the message to all connected clients
+            Clients.All.BroadcastMessage(user, message);
+        }
+    }
+}
